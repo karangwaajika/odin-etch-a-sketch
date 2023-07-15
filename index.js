56528f7a55e83cc1cdfe729e5d2652320c05b7c7
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-07-15 13:26:30 +0200

    add one square grid to container

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('grid');
+
+container.appendChild(content)
