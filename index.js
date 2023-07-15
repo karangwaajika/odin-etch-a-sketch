22fedce2df7791b839fb710a8682b0becd18ae4a
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-07-15 18:36:12 +0200

    add random background color on mouseover event from extra-credit task

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const userInput = document.querySelector('button.userInput');
 
 for(let i=1; i<= 256; i++){
     const div = document.createElement('div');
+    let red = Math.floor(Math.random() * (255-1 +1))+1;
+    let green = Math.floor(Math.random() * (255-1 +1))+1;
+    let blue = Math.floor(Math.random() * (255-1 +1))+1;
     div.classList.add('grid');
     div.classList.add(`grid-${i}`);
+    div.onmouseover = function(){div.setAttribute('style', `background-color:rgb(${red}, ${green}, ${blue})`)};
     container.appendChild(div)
 }
+
 
 userInput.addEventListener('click', () => {
     let gridSize = +prompt("Enter Grid Size");
