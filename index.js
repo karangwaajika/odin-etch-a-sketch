e1d63742eeed2bded93e1308a34f86779051ce66
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-07-15 14:22:55 +0200

    add 256 grids to make 16x16

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const container = document.querySelector('#container');
 
-const content = document.createElement('div');
-content.classList.add('grid');
 
-container.appendChild(content)
+for(let i=1; i<= 256; i++){
+    const content = document.createElement('div');
+    content.classList.add('grid');
+    content.classList.add(`grid-${i}`);
+    container.appendChild(content)
+}
